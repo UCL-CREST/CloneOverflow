7cf93d0c49107d16a54ea95d64619aa25c2c1c0d
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2017-02-18 15:21:51 +0000

    Updated the text in RQ1. Ready for Jens to look at.

--- cloneoverflow-latex/q_histogram.m
+++ cloneoverflow-latex/q_histogram.m
@@ -13,58 +13,58 @@ so_se_count = so_se(:,1);
 so_ne = csvread('/Users/Chaiyong/IdeasProjects/StackOverflowAnalyzer/stats_nicad_fse13_130901_1234_fixedclonelines/fragment_stats.csv', 1, 1);
 so_ne_count = so_ne(:,1);
 
-% subplot(1,2,1)
-% sosd_hist=histogram(so_sd_count,'NumBins', 60)
-% ylabel('No. of snippets','FontSize',16)
-% xlabel('No. of clone pairs','FontSize',16)
-% set(gca,'FontSize',16);
-% dim = [.19 .6 .3 .3];
-% str = 'NumBins=60, BinWidth=23';
-% annotation('textbox',dim,'String',str,'FitBoxToText','on','FontSize',15,'LineStyle','none');
-% 
-% subplot(1,2,2)
-% sdhist=histogram(sd,'NumBins', 60)
-% ylabel('No. of projects','FontSize',16)
-% xlabel('No. of clone pairs','FontSize',16)
-% set(gca,'FontSize',16);
-% dim = [.63 .6 .3 .3];
-% str = 'NumBins=60, BinWidth=266';
-% annotation('textbox',dim,'String',str,'FitBoxToText','on','FontSize',15,'LineStyle','none');
-% 
-% h=gcf;
-% set(h,'PaperPositionMode','auto');         
-% set(h,'PaperOrientation','landscape');
-% set(h,'Position',[50 50 800 300]);
-% print('hist_sd','-dpdf','-bestfit')
-
 subplot(1,2,1)
-sond_hist=histogram(so_nd_count,'NumBins',60)
+sosd_hist=histogram(so_sd_count,'NumBins', 60)
 ylabel('No. of snippets','FontSize',16)
-xlabel('No. of clone pairs','FontSize',16)
+xlabel('SO clone pairs','FontSize',16)
 set(gca,'FontSize',16);
 dim = [.19 .6 .3 .3];
-str = 'NumBins=60, BinWidth=110';
+str = 'NumBins=60, BinWidth=23';
 annotation('textbox',dim,'String',str,'FitBoxToText','on','FontSize',15,'LineStyle','none');
 
 subplot(1,2,2)
-ndhist=histogram(nd,'NumBins', 60)
+sdhist=histogram(sd,'NumBins', 60)
 ylabel('No. of projects','FontSize',16)
-xlabel('No. of clone pairs','FontSize',16)
+xlabel('Qualitas clone pairs','FontSize',16)
 set(gca,'FontSize',16);
-dim = [.62 .6 .3 .3];
-str = 'NumBins=60, BinWidth=5900';
+dim = [.63 .6 .3 .3];
+str = 'NumBins=60, BinWidth=266';
 annotation('textbox',dim,'String',str,'FitBoxToText','on','FontSize',15,'LineStyle','none');
 
 h=gcf;
 set(h,'PaperPositionMode','auto');         
 set(h,'PaperOrientation','landscape');
 set(h,'Position',[50 50 800 300]);
-print('hist_nd','-dpdf','-bestfit')
+print('hist_sd','-dpdf','-bestfit')
+
+% subplot(1,2,1)
+% sond_hist=histogram(so_nd_count,'NumBins',60)
+% ylabel('No. of snippets','FontSize',16)
+% xlabel('SO clone pairs','FontSize',16)
+% set(gca,'FontSize',16);
+% dim = [.19 .6 .3 .3];
+% str = 'NumBins=60, BinWidth=110';
+% annotation('textbox',dim,'String',str,'FitBoxToText','on','FontSize',15,'LineStyle','none');
+% 
+% subplot(1,2,2)
+% ndhist=histogram(nd,'NumBins', 60)
+% ylabel('No. of projects','FontSize',16)
+% xlabel('Qualitas clone pairs','FontSize',16)
+% set(gca,'FontSize',16);
+% dim = [.62 .6 .3 .3];
+% str = 'NumBins=60, BinWidth=5900';
+% annotation('textbox',dim,'String',str,'FitBoxToText','on','FontSize',15,'LineStyle','none');
+% 
+% h=gcf;
+% set(h,'PaperPositionMode','auto');         
+% set(h,'PaperOrientation','landscape');
+% set(h,'Position',[50 50 800 300]);
+% print('hist_nd','-dpdf','-bestfit')
 
 % subplot(1,2,1)
 % sose_hist=histogram(so_se_count,'NumBins', 60)
 % ylabel('No. of snippets','FontSize',16)
-% xlabel('No. of clone pairs','FontSize',16)
+% xlabel('SO clone pairs','FontSize',16)
 % set(gca,'FontSize',16);
 % dim = [.16 .6 .3 .3];
 % str = 'NumBins=60, BinWidth=640000';
@@ -73,7 +73,7 @@ print('hist_nd','-dpdf','-bestfit')
 % subplot(1,2,2)
 % sehist=histogram(se,'NumBins', 60)
 % ylabel('No. of projects','FontSize',16)
-% xlabel('No. of clone pairs','FontSize',16)
+% xlabel('Qualitas clone pairs','FontSize',16)
 % set(gca,'FontSize',16);
 % dim = [.6 .6 .3 .3];
 % str = 'NumBins=60, BinWidth=311000';
@@ -84,11 +84,11 @@ print('hist_nd','-dpdf','-bestfit')
 % set(h,'PaperOrientation','landscape');
 % set(h,'Position',[50 50 800 300]);
 % print('hist_se','-dpdf','-bestfit')
-% 
+
 % subplot(1,2,1);
 % sone_hist=histogram(so_ne_count,'NumBins', 60);
 % ylabel('No. of snippets','FontSize',16)
-% xlabel('No. of clone pairs','FontSize',16)
+% xlabel('SO clone pairs','FontSize',16)
 % set(gca,'FontSize',16);
 % dim = [.16 .6 .3 .3];
 % str = 'NumBins=60, BinWidth=12800';
@@ -97,7 +97,7 @@ print('hist_nd','-dpdf','-bestfit')
 % subplot(1,2,2);
 % nehist=histogram(ne,'NumBins', 60);
 % ylabel('No. of projects','FontSize',16);
-% xlabel('No. of clone pairs','FontSize',16)
+% xlabel('Qualitas clone pairs','FontSize',16)
 % set(gca,'FontSize',16);
 % dim = [.6 .6 .3 .3];
 % str = 'NumBins=60, BinWidth=455000';
