7b5bcecdf55ef3b0394d135517f536d0b99aa4c7
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2017-05-03 21:16:42 +0100

    Updated code to count the number of SO snippets per Qualitas file.

--- SnippetCounter/src/main/java/Main.java
+++ SnippetCounter/src/main/java/Main.java
@@ -6,14 +6,14 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
-        File file = new File("clones.csv");
+//        File file = new File("clones.csv");
+        File file = new File("clones_qs_ex_ud.csv");
         HashMap<String, HashSet<String>> cloneMap = new HashMap<String ,HashSet<String>>();
         HashMap<String, HashSet<String>> cloneMapNoLine = new HashMap<String ,HashSet<String>>();
 
         HashSet<String> snippets;
         HashSet<String> snippetsNoLine;
 
-        HashSet<String> qualitasClones = new HashSet<String>();
         int count = 0;
         // copied from
         // http://stackoverflow.com/questions/5868369/how-to-read-a-large-text-file-line-by-line-using-java
@@ -27,118 +27,88 @@ public class Main {
                     String[] lineSplit = line.split(",");
 
                     String qClone = "";
+                    String qCloneNoLine = "";
                     String soClone = "";
+                    String soCloneNoLine = "";
 
                     if (lineSplit.length >= 11) {
-                         qClone = lineSplit[3] + "$"
+                        qClone = lineSplit[3] + "$"
                                 + lineSplit[4] + "$"
                                 + lineSplit[5] + "$"
                                 + lineSplit[9] + "$"
                                 + lineSplit[10] + "$"
                                 + lineSplit[11];
-                         soClone = lineSplit[0] + "$"
-                                 + lineSplit[1] + "$"
-                                 + lineSplit[2] + "$"
-                                 + lineSplit[6] + "$"
-                                 + lineSplit[7] + "$"
-                                 + lineSplit[8];
+                        soClone = lineSplit[0] + "$"
+                                + lineSplit[1] + "$"
+                                + lineSplit[2] + "$"
+                                + lineSplit[6] + "$"
+                                + lineSplit[7] + "$"
+                                + lineSplit[8];
                     } else {
                         qClone = lineSplit[3] + "$"
                                 + lineSplit[4] + "$"
                                 + lineSplit[5];
+
                         soClone = lineSplit[0] + "$"
                                 + lineSplit[1] + "$"
                                 + lineSplit[2];
                     }
+                    qCloneNoLine = lineSplit[3];
+                    soCloneNoLine = lineSplit[0];
 
                     if (cloneMap.get(qClone) == null) {
                         snippets = new HashSet<String>();
-                        snippetsNoLine = new HashSet<String>();
-
                         snippets.add(soClone);
-                        snippetsNoLine.add(lineSplit[0]);
                     } else {
                         snippets = cloneMap.get(qClone);
-                        snippetsNoLine = cloneMapNoLine.get(qClone);
-
                         snippets.add(soClone);
-                        snippetsNoLine.add(lineSplit[0]);
+                    }
+
+                    if (cloneMapNoLine.get(qCloneNoLine) == null) {
+                        snippetsNoLine = new HashSet<String>();
+                        snippetsNoLine.add(soCloneNoLine);
+                    } else {
+                        snippetsNoLine = cloneMapNoLine.get(qCloneNoLine);
+                        snippetsNoLine.add(soCloneNoLine);
                     }
 
                     cloneMap.put(qClone, snippets);
-                    cloneMapNoLine.put(qClone, snippetsNoLine);
+                    cloneMapNoLine.put(qCloneNoLine, snippetsNoLine);
                 }
             }
 
             int totalQClones = cloneMap.size();
-            System.out.println("No. of distinct Qualitas clones: " + totalQClones + " out of " + count);
-
-            int multipleSnippetsCounter = 0;
-            int oneOneSnippetCounter = 0;
+            int totalQCloneNoLine = cloneMapNoLine.size();
             int totalCount = 0;
 
             Iterator it = cloneMapNoLine.entrySet().iterator();
 
             while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry)it.next();
+                Map.Entry pair = (Map.Entry) it.next();
                 HashSet<String> tmpSnippets = (HashSet<String>) pair.getValue();
-
-                if (tmpSnippets.size() > 1)
-                    multipleSnippetsCounter++;
-                else
-                    oneOneSnippetCounter++;
-
                 totalCount += tmpSnippets.size();
-
                 it.remove(); // avoids a ConcurrentModificationException
             }
 
             System.out.println("----------------");
-            System.out.println("Multiple snippet: " + multipleSnippetsCounter);
-            System.out.println("One snippet: " + oneOneSnippetCounter);
-            System.out.println("Total snippet: " + totalCount);
+
+            System.out.println("No. of distinct Qualitas file: " + totalQCloneNoLine + " out of " + count + " clone pairs.");
+            System.out.println("Average SO snippets per a Qualitas file: " + ((double) totalCount) / totalQCloneNoLine);
 
             it = cloneMap.entrySet().iterator();
             totalCount = 0;
-            int multipleSOSnippetCount = 0;
             int snippetPerQCount = 0;
 
             while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry)it.next();
+                Map.Entry pair = (Map.Entry) it.next();
                 HashSet<String> tmpSnippetsWithLines = (HashSet<String>) pair.getValue();
-
-                if (tmpSnippetsWithLines.size() > 1) {
-
-                    HashSet<String> soSnippetSet = new HashSet<String>();
-                     multipleSnippetsCounter++;
-
-                    for (String so: tmpSnippetsWithLines) {
-                        String[] tmp = so.split("\\$");
-                        // check if there are multiple snippets
-                        soSnippetSet.add(tmp[0]);
-                    }
-
-                    // System.out.println(soSnippetSet);
-
-                    // if multiple snippets
-                    if (soSnippetSet.size() > 1) {
-                        multipleSOSnippetCount++;
-                        snippetPerQCount += soSnippetSet.size();
-                    }
-                } else
-                    oneOneSnippetCounter++;
-
                 totalCount += tmpSnippetsWithLines.size();
-
                 it.remove(); // avoids a ConcurrentModificationException
             }
 
             System.out.println("----------------");
-            System.out.println("Multiple snippets per Q clone: " + multipleSnippetsCounter);
-            System.out.println("Multiple 'unique' snippets per Q clone: " + multipleSOSnippetCount);
-            System.out.println("Average SO snippets per Q clone: " + ((double) snippetPerQCount)/totalQClones);
-            System.out.println("One snippet: " + oneOneSnippetCounter);
-            System.out.println("Total snippets: " + totalCount);
+            System.out.println("No. of distinct Qualitas clones: " + totalQClones + " out of " + count + " clone pairs.");
+            System.out.println("Average SO clones per a Qualitas clone: " + ((double) totalCount)/totalQClones);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
