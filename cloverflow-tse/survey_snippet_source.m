34aea10d4784995f65a9fa68309f5448b6eb568e
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2017-08-22 23:09:10 +0100

    Updated the number in stacked bar chart.

--- cloverflow-tse/survey_snippet_source.m
+++ cloverflow-tse/survey_snippet_source.m
@@ -1,13 +1,19 @@
-group1 = [14 20 19 10 14 7; 3 5 9 0 14 53; 
-    5 6 16 13 16 28; 36 38 7 1 1 1; 
-    24 28 16 5 4 7; 1 2 14 6 17 44];
+group1 = [13 20 30 20 18; 
+    1 5 12 15 13; 
+    4 8 15 18 28; 
+    67 32 10 1 2; 
+    42 26 19 10 11; 
+    2 4 23 8 20];
 
-group2 = [13 20 30 19 18 11; 1 5 12 14 13 66; 
-    4 8 15 17 28 39; 66 32 10 1 2 0; 
-    42 26 18 10 11 4; 2 4 23 8 20 54];
+group2 = [14 20 19 10 14; 
+    3 5 9 0 14; 
+    5 6 16 13 16; 
+    36 38 7 1 1; 
+    24 28 16 5 4; 
+    1 2 14 6 17];
 
-bar_handle = bar(group1,'grouped');
-ylim([0 70])
+bar_handle = bar(group2,'stacked');
+ylim([0 120])
 ylabel('Amount','FontSize',16);
 xticklabels({'Personal projects', 'Company projects', 'Open source projects', 'Write from scratch', 'Modify from questions', 'Others'});
 xtickangle(30);
@@ -16,6 +22,6 @@ grid on
 
 l = cell(1,6);
 l{1}='Very frequently'; l{2}='Frequently'; l{3}='Occasionally'; l{4}='Rarely'; l{5}='Very rarely'; l{6}='Never';
-legend(bar_handle,l,'Location','Northwest','fontsize',12);
+legend(bar_handle,l,'Location','Northeast','fontsize',12);
 
 % print('survey_snippet_source','-dpdf','-bestfit')
