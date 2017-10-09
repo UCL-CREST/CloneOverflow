14e8b43733c4fcc61158633c8960d48a2d9e1a18
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2017-10-09 12:25:52 +0700

    Latest update on RQ1, 2, and 3.

---
+++ cloverflow-tse/ex_sources.m
@@ -0,0 +1,18 @@
+y = [63 18 15 11 3 1];
+bar(y)
+
+%xticks([1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19])
+xticklabels({'JDK', 'Web', 'OSS', 'Official docs', 'Book', 'Company'})
+%xlabel('Project','FontSize',15)
+ylabel('Clone pairs','FontSize',15)
+%t_legend=legend('Fresh', 'Outdated', 'Dead');
+%set(t_legend,'FontSize',15);
+set(gca,'FontSize',15);
+set(gca,'XTickLabelRotation',45)
+box on
+
+h=gcf;
+set(h,'PaperPositionMode','auto');         
+set(h,'PaperOrientation','landscape');
+set(h,'Position',[50 50 500 300]);
+print('ex_sources','-dpdf','-bestfit')
